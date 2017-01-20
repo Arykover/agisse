811128404f9f660e516d4aa4d39140e1ea782d25
Florian <Florian@pc-flo>
2017-01-20 12:27:01 +0100

    index configuration, bootstrap & jQuery addition

--- web/index.php
+++ web/index.php
@@ -1,11 +1,12 @@
-﻿<?php
+﻿
+    <?php
 // web/index.php
 require_once __DIR__.'/vendor/autoload.php';
 
 $app = new Silex\Application();
-
-// ... definitions
-
+require_once __DIR__.'/../app/services.php';
+require_once __DIR__.'/../app/routes.php';
+require_once __DIR__.'/../app/controleurs.php';
 $app->run();
 $app['debug'] = true;
 
