90478492e7cfe2a6db74175c82ee618d6e8dad22
Florian <Florian@pc-flo>
2017-01-21 10:50:10 +0100

    controleurs initialisation.

--- app/controleurs.php
+++ app/controleurs.php
@@ -5,12 +5,61 @@ use Symfony\Component\HttpFoundation\Request;
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Response;﻿
 
-//********************************************Contrôleur connexion*****************//
+//********************************************Contrôleur visiteur*****************//
 class ControleurVisiteur{
 
     public function __construct(){
         ob_start();             // démarre le flux de sortie
         require_once __DIR__.'/../views/v_entete.php';
     }
+    
+}
+
+
+//********************************************Contrôleur eleve*****************//
+class ControleurEleve{
+     private $id;
+     private $pdo;
+     public function init(){
+        $this->id = $_SESSION['id'];
+        $this->pdo = PdoGsb::getPdoAgisse();
+        ob_start();             // démarre le flux de sortie
+        require_once __DIR__.'/../vues/v_header.php';
+        require_once __DIR__.'/../vues/v_acceuil.php';
+        
+    }
+    
+    
+}
+
+//********************************************Contrôleur Gestionnaire*****************//
+class ControleurGestionnaire{
+     private $id;
+     private $pdo;
+     public function init(){
+        $this->id = $_SESSION['id'];
+        $this->pdo = PdoGsb::getPdoAgisse();
+        ob_start();             // démarre le flux de sortie
+        require_once __DIR__.'/../vues/v_header.php';
+        require_once __DIR__.'/../vues/v_acceuil.php';
+        
+    }
+    
+    
+}
+
+//********************************************Contrôleur Super User*****************//
+class ControleurSU{
+     private $id;
+     private $pdo;
+     public function init(){
+        $this->id = $_SESSION['id'];
+        $this->pdo = PdoGsb::getPdoAgisse();
+        ob_start();             // démarre le flux de sortie
+        require_once __DIR__.'/../vues/v_header.php';
+        require_once __DIR__.'/../vues/v_acceuil.php';
+        
+    }
+    
     
 }
