f5d73d63e379c2385f994c9737c564aeb16f6e98
Florian <Florian@pc-flo>
2017-01-24 18:05:00 +0100

    merge

--- app/services.php
+++ app/services.php
@@ -1,1 +1,19 @@
-﻿
+<?php
+use Symfony\Component\HttpFoundation\Request;
+use Silex\Application;
+
+
+class Services{
+    /**
+    * Détruit la session active
+    */
+    public function logOut(){
+		if (isset($_COOKIE[session_name()])) 
+		{
+			setcookie(session_name(), '', time()-42000, '/');
+		}
+		session_unset();
+		session_destroy();
+    }
+}
+?>
--- web/index.php
+++ web/index.php
@@ -5,8 +5,9 @@ require_once __DIR__.'/../vendor/autoload.php';
 date_default_timezone_set('Europe/Paris');
 $app = new Silex\Application();
 require_once __DIR__.'/../app/services.php';
+require_once __DIR__.'/../app/routes.php';
 require_once __DIR__.'/../app/controllers.php';
-require_once __DIR__.'/../app/roads.php';
+
 
 $app->run();
 $app['debug'] = true;
