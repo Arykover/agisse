b009e67ff545a53cef54bb817df3feb2a5a1e8b1
Fifine <Fifine@Fifine-PC>
2017-02-01 22:49:10 +0100

    config de htaccess

---
+++ web/views/v_maintenance.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of v_maintenance
+ *
+ * @author Fifine
+ */
+class v_maintenance {
+    //put your code here
+}
