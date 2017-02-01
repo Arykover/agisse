77ec2f99011b342e7d8cd314859c3ca6922e32f6
Arykover <Arykover@DESKTOP-5FSR1B3>
2017-02-01 16:43:08 +0100

    fiche visu

--- web/views/v_fiche.php
+++ web/views/v_fiche.php
@@ -5,60 +5,67 @@
     
     <tr>
         <td></td>
-        <td><?php if(isset($fiche['civilite'])){echo($civilite);}; ?></td>
+        <td><?php if(!empty($fiche['civilite'])){echo($civilite);}; ?></td>
         <td>Prenom : </td>
         <td><?php echo($_SESSION['prenom']); ?></td>
     </tr>
     
     <tr>
         <td>Nom d'usage :</td>
-        <td><?php if(isset($fiche[''])){echo($nom);}; ?></td>
+        <td><?php echo($_SESSION['nom']); ?></td>
         <td>Nom de naissance : (si different)</td>
-        <td><?php if(isset($fiche[''])){echo($nomNaiss);}; ?></td>
+        <td><?php if(!empty($fiche['nom_naiss'])){echo($fiche['nom_naiss']);}; ?></td>
     </tr>
     
     <tr>
-        <td>Lieu de naissance :</td>
-        <td><?php if(isset($fiche[''])){echo($lieuNaiss);}; ?></td>
-        <td>Date de naissance</td>
-        <td><?php if(isset($fiche[''])){echo($dateNaiss);}; ?></td>
+        <td>Commune de naissance :</td>
+        <td><?php if(!empty($fiche['commune_naissance'])){echo($fiche['commune_naissance']);}; ?></td>
+        <td>Departement de naissance</td>
+        <td><?php if(!empty($fiche['dept_naiss'])){echo($fiche['dept_naiss']);}; ?></td>
+    </tr>
+    
+    <tr>
+        <td>Date de naissance : </td>
+        <td><?php if(!empty($fiche['date_naiss'])){$fiche['date_naiss'];}; ?></td>
+        <td></td>
+        <td></td>
     </tr>
     
     <tr>
         <td>Discipline : </td>
-        <td><?php if(isset($fiche[''])){echo($discipline);}; ?></td>
+        <td><?php if(!empty($fiche['discipline'])){echo($fiche['discipline']);}; ?></td>
         <td></td>
         <td></td>
     </tr>
     
     <tr>
         <td>Nationalité :</td>
-        <td><?php if(isset($fiche[''])){echo($nationalite);}; ?></td>
+        <td><?php if(!empty($fiche['nationalite'])){echo($nationalite);}; ?></td>
         <td></td>
         <td></td>
     </tr>
     
     <tr>
         <td>Adresse : </td>
-        <td><?php if(isset($fiche[''])){echo($adresse);}; ?></td>
+        <td><?php if(!empty($fiche[''])){echo($adresse);}; ?></td>
     </tr>
     
     <tr>
         <td>Code postal :</td>
-        <td><?php if(isset($fiche[''])){echo($codePostal);}; ?></td>
+        <td><?php if(!empty($fiche[''])){echo($codePostal);}; ?></td>
         <td>Ville :</td>
-        <td><?php if(isset($fiche[''])){echo($ville);}; ?></td>
+        <td><?php if(!empty($fiche[''])){echo($ville);}; ?></td>
     </tr>
     
     <tr>
         <td>Numero de securité sociale :</td>
-        <td><?php if(isset($fiche[''])){echo($numSecu);}; ?></td>
+        <td><?php if(!empty($fiche[''])){echo($numSecu);}; ?></td>
     </tr>
     <tr>
         <td>Telephone :</td>
-        <td><?php if(isset($fiche[''])){echo($telephone);}; ?></td>
+        <td><?php if(!empty($fiche[''])){echo($telephone);}; ?></td>
         <td>Mail :</td>
-        <td><?php if(isset($fiche[''])){echo($mail);}; ?></td>
+        <td><?php if(!empty($fiche[''])){echo($mail);}; ?></td>
     </tr>
     
 </table>
@@ -69,11 +76,11 @@
     </tr>
     <tr>
         <td>Centre payeur :</td>
-        <td><?php if(isset($fiche[''])){echo($affiliation);}; ?></td>
+        <td><?php if(!empty($fiche[''])){echo($affiliation);}; ?></td>
     </tr>
     <tr>
         <td>Statut :</td>
-        <td><?php if(isset($fiche[''])){echo($statut);}; ?></td>
+        <td><?php if(!empty($fiche[''])){echo($statut);}; ?></td>
     </tr>
 </table>
 
@@ -83,10 +90,10 @@
     </tr>
     <tr>
         <td>Commentaire etudiant : </td>
-        <td><?php if(isset($fiche[''])){echo($commentEtudiant);}; ?></td>
+        <td><?php if(!empty($fiche[''])){echo($commentEtudiant);}; ?></td>
     </tr>
     <tr>
         <td>Commentaire gestionnaire : </td>
-        <td><?php if(isset($fiche[''])){echo($commentGestion);}; ?></td>
+        <td><?php if(!empty($fiche[''])){echo($commentGestion);}; ?></td>
     </tr>
 </table>
