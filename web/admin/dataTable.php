c36c08d3a10a79a6bf8cb2f1d227cdfdae574317
Fifine <Fifine@Fifine-PC>
2017-02-13 19:25:56 +0100

    datatable etablissement, part 4,  reconfig pour dynamique, transit variable

--- web/admin/dataTable.php
+++ web/admin/dataTable.php
@@ -1,92 +1,176 @@
 <?php
-/*
- * For more details
- * please check official documentation of DataTables  https://datatables.net/manual/server-side
- * Coded by charaf JRA
- * RefreshMyMind.com
+/**
+ * Script:    DataTables server-side script for PHP 5.2+ and MySQL 4.1+
+ * Notes:     Based on a script by Allan Jardine that used the old PHP mysql_* functions.
+ *            Rewritten to use the newer object oriented mysqli extension.
+ * Copyright: 2010 - Allan Jardine (original script)
+ *            2012 - Kari Söderholm, aka Haprog (updates)
+ * License:   GPL v2 or BSD (3-point)
  */
-
-/* IF Query comes from DataTables do the following */
-if (!empty($_POST) ) {
-
-    /*
-     * Database Configuration and Connection using mysqli
-     */
-
-    define("HOST", "localhost");
-    define("USER", "root");
-    define("PASSWORD", "root");
-    define("DB", "gisse");
-    define("MyTable", "info_etablissmeent");
-
-    $connection = mysqli_connect(HOST, USER, PASSWORD, DB) OR DIE("Impossible to access to DB : " . mysqli_connect_error());
-
-    /* END DB Config and connection */
-
-    /*
-     * @param (string) SQL Query
-     * @return multidim array containing data array(array('column1'=>value2,'column2'=>value2...))
-     *
-     */
-    function getData($sql){
-        global $connection ;//we use connection already opened
-        $query = mysqli_query($connection, $sql) OR DIE ("Can't get Data from DB , check your SQL Query " );
-        $data = array();
-        foreach ($query as $row ) {
-            $data[] = $row ;
-        }
-        return $data;
-    }
-
-    /* Useful $_POST Variables coming from the plugin */
-    $draw = $_POST["draw"];//counter used by DataTables to ensure that the Ajax returns from server-side processing requests are drawn in sequence by DataTables
-    $orderByColumnIndex  = $_POST['order'][0]['column'];// index of the sorting column (0 index based - i.e. 0 is the first record)
-    $orderBy = $_POST['columns'][$orderByColumnIndex]['data'];//Get name of the sorting column from its index
-    $orderType = $_POST['order'][0]['dir']; // ASC or DESC
-    $start  = $_POST["start"];//Paging first record indicator.
-    $length = $_POST['length'];//Number of records that the table can display in the current draw
-    /* END of POST variables */
-
-    $recordsTotal = count(getData("SELECT * FROM ".MyTable));
-
-    /* SEARCH CASE : Filtered data */
-    if(!empty($_POST['search']['value'])){
-
-        /* WHERE Clause for searching */
-        for($i=0 ; $i<count($_POST['columns']);$i++){
-            $column = $_POST['columns'][$i]['data'];//we get the name of each column using its index from POST request
-            $where[]="$column like '%".$_POST['search']['value']."%'";
-        }
-        $where = "WHERE ".implode(" OR " , $where);// id like '%searchValue%' or name like '%searchValue%' ....
-        /* End WHERE */
-
-        $sql = sprintf("SELECT * FROM %s %s", MyTable , $where);//Search query without limit clause (No pagination)
-
-        $recordsFiltered = count(getData($sql));//Count of search result
-
-        /* SQL Query for search with limit and orderBy clauses*/
-        $sql = sprintf("SELECT * FROM %s %s ORDER BY %s %s limit %d , %d ", MyTable , $where ,$orderBy, $orderType ,$start,$length  );
-        $data = getData($sql);
-    }
-    /* END SEARCH */
-    else {
-        $sql = sprintf("SELECT * FROM %s ORDER BY %s %s limit %d , %d ", MyTable ,$orderBy,$orderType ,$start , $length);
-        $data = getData($sql);
-
-        $recordsFiltered = $recordsTotal;
-    }
-
-    /* Response to client before JSON encoding */
-    $response = array(
-        "draw" => intval($draw),
-        "recordsTotal" => $recordsTotal,
-        "recordsFiltered" => $recordsFiltered,
-        "data" => $data
-    );
-
-    echo json_encode($response);
-
-} else {
-    echo "NO POST Query from DataTable";
+mb_internal_encoding('UTF-8');
+    
+/**
+ * Array of database columns which should be read and sent back to DataTables. Use a space where
+ * you want to insert a non-database field (for example a counter or static image)
+ */
+$aColumns = array( 'id', 'denomination', 'caisse_prim', 'n_agrement', 'annee_scolaire', 'code_grand_regime' );
+    
+// Indexed column (used for fast and accurate table cardinality)
+$sIndexColumn = 'id';
+    
+// DB table to use
+	$sTable = "info_etablissmeent";
+            
+// Database connection information
+$gaSql['user']     = 'root';
+$gaSql['password'] = 'root';
+$gaSql['db']       = 'gisse';
+$gaSql['server']   = 'localhost';
+$gaSql['port']     = 3306; // 3306 is the default MySQL port
+    
+// Input method (use $_GET, $_POST or $_REQUEST)
+$input =& $_GET;
+    
+/** * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * If you just want to use the basic configuration for DataTables with PHP server-side, there is
+ * no need to edit below this line
+ */
+     
+/**
+ * Character set to use for the MySQL connection.
+ * MySQL will return all strings in this charset to PHP (if the data is stored correctly in the database).
+ */
+$gaSql['charset']  = 'utf8';
+    
+/**
+ * MySQL connection
+ */
+$db = new mysqli($gaSql['server'], $gaSql['user'], $gaSql['password'], $gaSql['db'], $gaSql['port']);
+if (mysqli_connect_error()) {
+    die( 'Error connecting to MySQL server (' . mysqli_connect_errno() .') '. mysqli_connect_error() );
 }
+    
+if (!$db->set_charset($gaSql['charset'])) {
+    die( 'Error loading character set "'.$gaSql['charset'].'": '.$db->error );
+}
+    
+    
+/**
+ * Paging
+ */
+$sLimit = "";
+if ( isset( $input['iDisplayStart'] ) && $input['iDisplayLength'] != '-1' ) {
+    $sLimit = " LIMIT ".intval( $input['iDisplayStart'] ).", ".intval( $input['iDisplayLength'] );
+}
+    
+    
+/**
+ * Ordering
+ */
+$aOrderingRules = array();
+if ( isset( $input['iSortCol_0'] ) ) {
+    $iSortingCols = intval( $input['iSortingCols'] );
+    for ( $i=0 ; $i<$iSortingCols ; $i++ ) {
+        if ( $input[ 'bSortable_'.intval($input['iSortCol_'.$i]) ] == 'true' ) {
+            $aOrderingRules[] =
+                "`".$aColumns[ intval( $input['iSortCol_'.$i] ) ]."` "
+                .($input['sSortDir_'.$i]==='asc' ? 'asc' : 'desc');
+        }
+    }
+}
+    
+if (!empty($aOrderingRules)) {
+    $sOrder = " ORDER BY ".implode(", ", $aOrderingRules);
+} else {
+    $sOrder = "";
+}
+    
+    
+/**
+ * Filtering
+ * NOTE this does not match the built-in DataTables filtering which does it
+ * word by word on any field. It's possible to do here, but concerned about efficiency
+ * on very large tables, and MySQL's regex functionality is very limited
+ */
+$iColumnCount = count($aColumns);
+    
+if ( isset($input['sSearch']) && $input['sSearch'] != "" ) {
+    $aFilteringRules = array();
+    for ( $i=0 ; $i<$iColumnCount ; $i++ ) {
+        if ( isset($input['bSearchable_'.$i]) && $input['bSearchable_'.$i] == 'true' ) {
+            $aFilteringRules[] = "`".$aColumns[$i]."` LIKE '%".$db->real_escape_string( $input['sSearch'] )."%'";
+        }
+    }
+    if (!empty($aFilteringRules)) {
+        $aFilteringRules = array('('.implode(" OR ", $aFilteringRules).')');
+    }
+}
+    
+// Individual column filtering
+for ( $i=0 ; $i<$iColumnCount ; $i++ ) {
+    if ( isset($input['bSearchable_'.$i]) && $input['bSearchable_'.$i] == 'true' && $input['sSearch_'.$i] != '' ) {
+        $aFilteringRules[] = "`".$aColumns[$i]."` LIKE '%".$db->real_escape_string($input['sSearch_'.$i])."%'";
+    }
+}
+    
+if (!empty($aFilteringRules)) {
+    $sWhere = " WHERE ".implode(" AND ", $aFilteringRules);
+} else {
+    $sWhere = "";
+}
+    
+    
+/**
+ * SQL queries
+ * Get data to display
+ */
+$aQueryColumns = array();
+foreach ($aColumns as $col) {
+    if ($col != ' ') {
+        $aQueryColumns[] = $col;
+    }
+}
+    
+$sQuery = "
+    SELECT SQL_CALC_FOUND_ROWS `".implode("`, `", $aQueryColumns)."`
+    FROM `".$sTable."`".$sWhere.$sOrder.$sLimit;
+        
+$rResult = $db->query( $sQuery ) or die($db->error);
+    
+// Data set length after filtering
+$sQuery = "SELECT FOUND_ROWS()";
+$rResultFilterTotal = $db->query( $sQuery ) or die($db->error);
+list($iFilteredTotal) = $rResultFilterTotal->fetch_row();
+    
+// Total data set length
+$sQuery = "SELECT COUNT(`".$sIndexColumn."`) FROM `".$sTable."`";
+$rResultTotal = $db->query( $sQuery ) or die($db->error);
+list($iTotal) = $rResultTotal->fetch_row();
+    
+    
+/**
+ * Output
+ */
+$output = array(
+//    "sEcho"                => intval($input['sEcho']),
+    "iTotalRecords"        => $iTotal,
+    "iTotalDisplayRecords" => $iFilteredTotal,
+    "aaData"               => array(),
+);
+    
+while ( $aRow = $rResult->fetch_assoc() ) {
+    $row = array();
+    for ( $i=0 ; $i<$iColumnCount ; $i++ ) {
+        if ( $aColumns[$i] == 'version' ) {
+            // Special output formatting for 'version' column
+            $row[] = ($aRow[ $aColumns[$i] ]=='0') ? '-' : $aRow[ $aColumns[$i] ];
+        } elseif ( $aColumns[$i] != ' ' ) {
+            // General output
+            $row[] = $aRow[ $aColumns[$i] ];
+        }
+    }
+    $output['aaData'][] = $row;
+}
+    
+echo json_encode( $output );	
 ?>
