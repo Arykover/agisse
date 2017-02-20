cb921379aaa12f467b31289d6487e9c789eb44ab
Fifine <Fifine@192.168.1.246>
2017-02-20 11:04:57 +0100

    formEditDatatable - edition: save delete add

--- web/admin/views/v_formEditDatatable.php
+++ web/admin/views/v_formEditDatatable.php
@@ -1,15 +1,14 @@
  
     <?php
+    
+//           $infos = ($_POST['infos']);
 //        $fName = $userInfo['nom'];
 //        $lName = $userInfo['prenom'];
 //        $mail = $userInfo['mail'];
         ?>
-        <input type='hidden' name='id' value='<?php echo 'hehe' ?>'>
-    <button type="submit" id="sub" class="btn btn-default">valider</button>
-    <!--onclick="SubmitCheckSecu()-->
 </form>
 <!-- Button trigger modal -->
-<button id='trigger' type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal" style='display:none;'>
+<button id='trigger' type="submit" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal" style='display:none;' >
   Modal Trigger Button
 </button>
 
@@ -22,33 +21,28 @@
         <h4 class="modal-title" id="myModalLabel">Modal title</h4>
       </div>
       <div class="modal-body">
-          <form id='FormDataTab' method='POST' action='update' >
+          <form id='FormDataTab' method='POST' >
               <!--//style='display:none;'-->
         <table class='table table-condensed'>
-            <!--halouf-->
-            <?php 
-//            $infos = $_POST['infos'];
-            echo $_POST['sTable'];
-            if ( (isset($_POST['infos'])) ) {echo 'oui';};?>
             <?php 
             foreach ($columnsName as $c)
             {
             ?>
             <tr>
                 <td> <?php echo $c['column_name'];?> </td>
-                <!--<td> <input type='text'   value='<?php // if(!empty($VARIABLERECUP[$NumLineSelect])){echo($fiche['commune_naiss']);}; ?>'></td>-->
-                <td> <input type='text'   value=''></td>
+                <td> <input type='text' name="titles[]" value=''></td>
             </tr>
             <?php
             }
             ?>
         </table>
-          </form>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="submit" class="btn btn-primary" id="save">Save changes</button>
       </div>
+          </form>
     </div>
   </div>
 </div>
+<?php // echo json_encode( $infos ); ?>
